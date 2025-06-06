fdb8836829fb766411ec5f85b819e84dd74d3229
= <=>
2025-06-07 01:11:35 +0300

    implement py-game-models

--- main.py
+++ main.py
@@ -1,52 +1,40 @@
-from db.models import Guild, Player, Race, Skill
 import json
-from datetime import datetime
-
+from models import Race, Guild, Skill, Player  # Імпортуй свої моделі
 
 def main() -> None:
     with open("players.json", "r") as file:
-        player_list = json.load(file)
+        players = json.load(file)
 
-    for player_data in player_list:
+    for player_data in players.values():
+        # Race
         race_obj, _ = Race.objects.get_or_create(
             name=player_data["race"]["name"],
-            defaults={
-                "description": player_data["race"]["description"],
-            },
+            defaults={"description": player_data["race"]["description"]}
         )
 
-        for skill in player_data["race"]["skills"]:
+        # Skills
+        for skill in player_data["race"].get("skills", []):
             Skill.objects.get_or_create(
                 name=skill["name"],
-                defaults={
-                    "bonus": skill["bonus"],
-                    "race": race_obj,
-                },
+                bonus=skill["bonus"],
+                race=race_obj
             )
 
+        # Guild
         guild_data = player_data.get("guild")
         guild_obj = None
         if guild_data:
             guild_obj, _ = Guild.objects.get_or_create(
                 name=guild_data["name"],
-                defaults={
-                    "description": guild_data["description"],
-                },
+                defaults={"description": guild_data["description"]}
             )
 
+        # Player
         Player.objects.get_or_create(
-            nickname=player_data["nickname"],
+            email=player_data["email"],
             defaults={
-                "email": player_data["email"],
                 "bio": player_data["bio"],
                 "race": race_obj,
                 "guild": guild_obj,
-                "created_at": datetime.fromisoformat(
-                    player_data["created_at"]
-                ),
-            },
+            }
         )
-
-
-if __name__ == "__main__":
-    main()
