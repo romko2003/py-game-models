263379b7df1fdcca54834418e912325737a437b4
= <=>
2025-06-07 02:04:07 +0300

    implement py-game-models

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main() -> None:
         # Отримуємо або створюємо расу
         race_obj, _ = Race.objects.get_or_create(
             name=player_data["race"]["name"],
-            defaults={"description":player_data["race"].get(
+            defaults={"description": player_data["race"].get(
                 "description", ""
             )},
         )
